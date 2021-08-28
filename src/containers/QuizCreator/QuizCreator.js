e0f09b526ddcaab0b6503216bad7796412edf570
andrew-savich <savichandrew@Gmail.com>
2021-08-28 12:57:34 +0300

    Validate create quiz form

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -3,7 +3,7 @@ import classes from './QuizCreator.module.css'
 import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
 import Select from '../../components/UI/Select/Select'
-import {createControl, validate} from '../../form/formFramework'
+import {createControl, validate, validateForm} from '../../form/formFramework'
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary'
 
 function createOptionControl(number){
@@ -11,7 +11,7 @@ function createOptionControl(number){
     label: `Option ${number}`,
     errorMessage: 'Option cant be empty',
     id: number
-  })
+  }, {required: true})
 }
 
 function createFormControls(){
@@ -37,11 +37,11 @@ export default class QuizCreator extends Component {
   }
 
   submitHandler = event => {
-    event.preventDfault()
+    event.preventDefault()
   }
 
-  addQuestionHandler = () => {
-
+  addQuestionHandler = event => {
+    event.preventDefault()
   }
 
   addQuestionHandler = () => {
@@ -54,13 +54,17 @@ export default class QuizCreator extends Component {
 
     control.touched = true
     control.value = value
-    control.valid = validate(control.value, control.valiidation)
+    control.valid = validate(control.value, control.validation)
 
     formControls[controlName] = control
 
+    this.setState({
+      formControls,
+      isFormValid: validateForm(formControls)
+    })
   }
 
-  renderControls(){
+  renderControls() {
     return Object.keys(this.state.formControls).map((controlName, index) => {
       const control = this.state.formControls[controlName]
 
@@ -70,12 +74,12 @@ export default class QuizCreator extends Component {
             label={control.label}
             value={control.value}
             valid={control.valid}
-            shouldValidate={!!control.valiidation}
+            shouldValidate={!!control.validation}
             touched={control.touched}
             errorMessage={control.errorMessage}
-            onChange={event => this.changeHandler(event.target.value, controlName) }
+            onChange={event => this.changeHandler(event.target.value, controlName)}
           />
-          { index === 0 ? <br /> : null}
+          { index === 0 ? <hr /> : null }
         </Auxiliary>
       )
     })
@@ -114,6 +118,7 @@ export default class QuizCreator extends Component {
               <Button
                 type="primary"
                 onClick={this.addQuestionHandler}
+                disabled={!this.state.isFormValid}
               >
                 Add question
               </Button>
@@ -121,6 +126,7 @@ export default class QuizCreator extends Component {
               <Button
                 type="success"
                 onClick={this.addCreateQuizHandler}
+                disabled={this.state.quiz.length === 0}
               >
                 Create test
               </Button>
--- src/form/formFramework.js
+++ src/form/formFramework.js
@@ -9,15 +9,27 @@ export function createControl(config, validation) {
 }
 
 export function validate(value, validation = null) {
-    if(!validation){
-        return true
+    if (!validation) {
+      return true
     }
-
+  
     let isValid = true
-
+  
     if (validation.required) {
-        isValid = value.trim() !== '' && isValid
+      isValid = value.trim() !== '' && isValid
     }
+  
+    return isValid
+  } 
 
-    return osValid
-}
+  export function validateForm(formControls) {
+    let isFormValid = true
+  
+    for (let control in formControls) {
+      if (formControls.hasOwnProperty(control)) {
+        isFormValid = formControls[control].valid && isFormValid
+      }
+    }
+  
+    return isFormValid
+  }
